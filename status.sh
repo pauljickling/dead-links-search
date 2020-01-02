1ed5d5f6bfa187d84b48c697c14bbf35be25e915
Paul Jickling <pjickling@gmail.com>
2020-01-02 07:16:08 -0500

    Condensed sed operations to one line

--- status.sh
+++ status.sh
@@ -3,11 +3,8 @@ set -eu
 echo "Retrieving links"
 node get_links.js > links
 echo "Modifying links file"
-sed -i.bak "s/\[//g" links
-sed -i.bak "s/\]//g" links
-sed -i.bak "s/\,//g" links
-sed -i.bak "s/'//g" links
-echo "Checking HTTP status codes of links (this may take a minute)"
+sed -i.bak "s/[\[\]\,\']//g" links
+echo "Checking HTTP status codes of links"
 echo "status_code, url" > status.csv
 if [[ -e ./links ]] ; then
     while read -r i; do
